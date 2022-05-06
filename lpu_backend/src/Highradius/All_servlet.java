e94821f257eed9951cb0cc691a5b05808a94d0c3
kartikeyshaurya <kartikeyshaurya@gmail.com>
2022-05-06 05:35:39 +0530

    tried for adding  new servlet

---
+++ lpu_backend/src/Highradius/All_servlet.java
@@ -0,0 +1,39 @@
+package Highradius;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class All_servlet
+ */
+@WebServlet("/All_servlet")
+public class All_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public All_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
