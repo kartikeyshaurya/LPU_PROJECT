0fc9ed1d4d7cf414a25156a130713d5c83549153
kartikeyshaurya <kartikeyshaurya@gmail.com>
2022-05-07 05:36:20 +0530

    classes for java

---
+++ lpu_backend/src/Highradius/ClassDetails.java
@@ -0,0 +1,120 @@
+package Highradius;
+
+public class ClassDetails {
+
+	
+
+	
+	private String business_code ; 
+	private String cust_number; 
+	private String name_customer ;
+	private float buisness_year ;
+	private String posting_date ;
+	private int document_create_date ;
+
+	private float  due_in_date ;
+	private String invoice_currency ;
+	
+	//private String document type' ;
+	private float posting_id ;
+	private String area_business ;
+	private float total_open_amount ;
+	private float baseline_create_date ;
+	private String cust_payment_terms ;
+	private String invoice_id ;
+	private int isOpen ;
+	public String getBusiness_code() {
+		return business_code;
+	}
+	public void setBusiness_code(String business_code) {
+		this.business_code = business_code;
+	}
+	public String getCust_number() {
+		return cust_number;
+	}
+	public void setCust_number(String cust_number) {
+		this.cust_number = cust_number;
+	}
+	public String getName_customer() {
+		return name_customer;
+	}
+	public void setName_customer(String name_customer) {
+		this.name_customer = name_customer;
+	}
+	public float getBuisness_year() {
+		return buisness_year;
+	}
+	public void setBuisness_year(float buisness_year) {
+		this.buisness_year = buisness_year;
+	}
+	public String getPosting_date() {
+		return posting_date;
+	}
+	public void setPosting_date(String posting_date) {
+		this.posting_date = posting_date;
+	}
+	public int getDocument_create_date() {
+		return document_create_date;
+	}
+	public void setDocument_create_date(int document_create_date) {
+		this.document_create_date = document_create_date;
+	}
+	public float getDue_in_date() {
+		return due_in_date;
+	}
+	public void setDue_in_date(float due_in_date) {
+		this.due_in_date = due_in_date;
+	}
+	public String getInvoice_currency() {
+		return invoice_currency;
+	}
+	public void setInvoice_currency(String invoice_currency) {
+		this.invoice_currency = invoice_currency;
+	}
+	public float getPosting_id() {
+		return posting_id;
+	}
+	public void setPosting_id(float posting_id) {
+		this.posting_id = posting_id;
+	}
+	public String getArea_business() {
+		return area_business;
+	}
+	public void setArea_business(String area_business) {
+		this.area_business = area_business;
+	}
+	public float getTotal_open_amount() {
+		return total_open_amount;
+	}
+	public void setTotal_open_amount(float total_open_amount) {
+		this.total_open_amount = total_open_amount;
+	}
+	public float getBaseline_create_date() {
+		return baseline_create_date;
+	}
+	public void setBaseline_create_date(float baseline_create_date) {
+		this.baseline_create_date = baseline_create_date;
+	}
+	public String getCust_payment_terms() {
+		return cust_payment_terms;
+	}
+	public void setCust_payment_terms(String cust_payment_terms) {
+		this.cust_payment_terms = cust_payment_terms;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public int getIsOpen() {
+		return isOpen;
+	}
+	public void setIsOpen(int isOpen) {
+		this.isOpen = isOpen;
+	} 
+	
+	
+	
+
+}
