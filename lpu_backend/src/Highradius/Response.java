3376eef93979ad26f751c21916739c6e7e740b29
kartikeyshaurya <kartikeyshaurya@gmail.com>
2022-05-09 05:38:34 +0530

    status watch file

---
+++ lpu_backend/src/Highradius/Response.java
@@ -0,0 +1,19 @@
+package Highradius;
+
+public class Response {
+
+	String status ; 
+	Boolean success ;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Boolean getSuccess() {
+		return success;
+	}
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	} 
+}
